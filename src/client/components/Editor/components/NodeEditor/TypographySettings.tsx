fabf1292e2867deddb68f60a6b3d7ff41914f6d1
Steve Chester <schester44@gmail.com>
2022-01-17 23:02:14 -0500

    font picker

--- src/client/components/Editor/components/NodeEditor/TypographySettings.tsx
+++ src/client/components/Editor/components/NodeEditor/TypographySettings.tsx
@@ -1,12 +1,12 @@
-import { Menu } from "@headlessui/react";
 import React from "react";
 import { RiAlignCenter, RiAlignJustify, RiAlignLeft, RiAlignRight } from "react-icons/ri";
 import { useRecoilState } from "recoil";
 import { nodesState } from "../../recoil/atoms";
 import { Node } from "../../types";
-import { Dropdown, MenuItem } from "../Dropdown";
 import { IconButton } from "../IconButton";
 import { ColorPicker } from "../inputs/ColorPicker";
+import FontPicker from "../inputs/FontPicker";
+import { Input } from "../inputs/Input";
 import { CollapsiblePanel } from "./CollapsiblePanel";
 import { PanelSection } from "./CollapsiblePanel/PanelSection";
 import { Setting } from "./CollapsiblePanel/Setting";
@@ -36,26 +36,51 @@ export const TypographySettings = ({ nodeId }: Props) => {
     <CollapsiblePanel title="Typography">
       <PanelSection title="Style">
         <Setting name="Font">
-          use select here, not dropdown
-          <Dropdown
-            dropdownClassNames="w-full"
-            content={
-              <>
-                <MenuItem>Times New Roman</MenuItem>
-                <MenuItem>Ubuntu Pro</MenuItem>
-                <MenuItem>Ubuntu Pro Bold</MenuItem>
-                <MenuItem>Ubuntu Pro Italic</MenuItem>
-                <MenuItem>Random Font</MenuItem>
-                <MenuItem>Random Font Bold</MenuItem>
-              </>
+          <FontPicker
+            value={styles.fontFamily}
+            onChange={(fontFamily) =>
+              setStyle({
+                fontFamily,
+              })
             }
-          >
-            <Menu.Button className="bg-gray-600 hover:bg-gray-700 text-xs px-2 py-1 w-full text-left rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-indigo-500 hover:text-white">
-              Times New Roman
-            </Menu.Button>
-          </Dropdown>
+          />
         </Setting>
-        <Setting name="Size">Font Size, Line height, spacing</Setting>
+        <Setting name="Size">
+          <div className="flex">
+            <div className="w-1/3">
+              <Input
+                type="number"
+                value={styles.fontSize}
+                onChange={({ target: { value } }) =>
+                  setStyle({ fontSize: isNaN(Number(value)) ? "" : Number(value) })
+                }
+              />
+              <div className="text-[9px] text-center text-gray-500">Font size</div>
+            </div>
+
+            <div className="w-1/3 px-2">
+              <Input
+                type="number"
+                value={styles.lineHeight}
+                onChange={({ target: { value } }) =>
+                  setStyle({ lineHeight: isNaN(Number(value)) ? "" : Number(value) })
+                }
+              />
+              <div className="text-[9px] text-center text-gray-500">Line height</div>
+            </div>
+
+            <div className="w-1/3">
+              <Input
+                type="number"
+                value={styles.letterSpacing}
+                onChange={({ target: { value } }) =>
+                  setStyle({ letterSpacing: isNaN(Number(value)) ? "" : Number(value) })
+                }
+              />
+              <div className="text-[9px] text-center text-gray-500">Letter spacing</div>
+            </div>
+          </div>
+        </Setting>
         <Setting name="Color">
           <ColorPicker
             value={styles.color}
---
+++ src/client/components/Editor/components/inputs/FontPicker/index.tsx
@@ -0,0 +1,52 @@
+import { Listbox, Transition } from "@headlessui/react";
+
+type Props = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+const fontMap = {
+  "Ubuntu Mono": "Ubuntu Mono",
+  "Roboto Mono": "Roboto Mono",
+};
+
+const fonts = Object.keys(fontMap);
+
+export const FontPicker: React.FC<Props> = ({ value, onChange }) => {
+  return (
+    <div className="z-50 relative">
+      <Listbox value={value} onChange={onChange}>
+        <Listbox.Button className="rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-900 focus:ring-indigo-500 hover:text-white text-sm bg-gray-800 w-full text-left py-1 px-2">
+          {fontMap[value as keyof typeof fontMap] || fontMap[fonts[0]]}
+        </Listbox.Button>
+
+        <Transition
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-50"
+        >
+          <Listbox.Options className="absolute rounded-md shadow-lg bg-white text-white ring-1 ring-black ring-opacity-5 focus:outline-none overflow-hidden mt-2">
+            {fonts.map((key) => {
+              const font = fontMap[key as keyof typeof fontMap];
+
+              return (
+                <Listbox.Option
+                  key={key}
+                  value={key}
+                  className="text-gray-900 hover:bg-gray-100 px-2 py-2 text-xs block cursor-pointer"
+                >
+                  {font}
+                </Listbox.Option>
+              );
+            })}
+          </Listbox.Options>
+        </Transition>
+      </Listbox>
+    </div>
+  );
+};
+
+export default FontPicker;
